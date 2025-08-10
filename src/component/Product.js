29af54556db63cba94562d92923c00818c37ebc8
Abhishek Singh <abhishekrbs.singh@gmail.com>
2025-08-10 14:48:37 +0530

    Changed images and components

--- src/component/Product.js
+++ src/component/Product.js
@@ -1,25 +1,25 @@
 import React from 'react'
-import product1 from '../assets/img/menu/potato-flakes.webp';
-import product2 from '../assets/img/menu/milled_potato.webp'
+import product1 from '../assets/img/menu/product1.png';
+import product2 from '../assets/img/menu/milled-product.png'
 
 export default function Product() {
     return (
         <div className='container'  style={{width: '800px'}}>
-            <h3 style={{ textAlign: 'center' }}>Find Out the Most Popular Product</h3>
+            <h1 style={{ textAlign: 'center'  }}>Find Out the Most Popular Product</h1>
             <div class="card-group">
-                <div class="card mx-5 my-4">
+                <div class="card mx-5 my-4"  style={{ border:'none'}}>
                     <a href='assests/img/menu/potato-flakes'><img src={product1} class="card-img-top" alt="..." /></a>
 
                     <div class="card-body">
-                        <h5 class="card-title">Potato Flakes</h5>
+                        <h3 class="card-title">Potato Flakes</h3>
                         <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
                         <a href="#" class="btn btn-primary">Read More</a>
                     </div>
                 </div>
-                <div class="card my-4">
+                <div class="card my-4"  style={{ border:'none'}}>
                      <a href='assests/img/menu/milled-potato'><img src={product2} class="card-img-top" alt="..." /></a>
                     <div class="card-body">
-                        <h5 class="card-title">Milled Products</h5>
+                        <h3 class="card-title">Milled Products</h3>
                         <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
                        <a href="#" class="btn btn-primary">Read More</a>
                     </div>
