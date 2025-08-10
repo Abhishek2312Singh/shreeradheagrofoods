f7fdcbbefd7af04c2d77dd70ecfe73d681baeae6
Tanisha Nainwal <tanishanainwal.5@gmail..com>
2025-08-10 14:50:47 +0530

    multiple changes

--- src/component/Hero.js
+++ src/component/Hero.js
@@ -11,7 +11,7 @@ function Hero() {
             <h1  >Enjoy Your Healthy<br/>Delicious Food</h1>
             <p   data-aos-delay="100">We are team of talented designers making websites with Bootstrap</p>
             <div  className= "d-flex"   data-aos-delay="200">
-              <a href="#manufacture"  className= "btn-get-started underline">Now More</a>
+              <a href="#manufacture"  className= "btn-get-started underline">Know More</a>
               <a href="https://www.youtube.com/watch?v=91QYV47fsBA" target='_blank' className= "glightbox btn-watch-video d-flex align-items-center underline"><i  className= "bi bi-play-circle"></i><span>Watch Video</span></a>
             </div>
           </div>
